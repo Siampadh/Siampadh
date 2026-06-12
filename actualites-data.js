88015c872a16e7cb0f42e980b9bf548eab7a83b0
Siampadh <siampadh77@essaimgatinais.asso.fr>
2026-06-12 12:54:31 +0200

    Add files via upload

---
+++ actualites-data.js
@@ -0,0 +1,15 @@
+// MODIFICATION DES ACTUALITÉS DEPUIS GITHUB
+// Pour ajouter une photo :
+// 1) Ajoutez votre photo dans assets/actualites/
+// 2) Indiquez le chemin, exemple : photo: 'assets/actualites/barbecue.jpg'
+// Pour ajouter une actualité, copiez/collez un bloc complet entre { ... },
+// en pensant à mettre une virgule entre les blocs.
+
+window.NEWS_DATA = [
+  {
+    title: 'Bienvenue sur les actualités du SIAMPADH',
+    date: '',
+    text: 'Cette page permet de partager les informations importantes, les événements, les sorties et les moments de vie de l’association.',
+    photo: ''
+  }
+];
