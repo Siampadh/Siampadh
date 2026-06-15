e5f8637f1f96ab696d6262ae17027fed552140b2
Siampadh <siampadh77@essaimgatinais.asso.fr>
2026-06-15 16:20:52 +0200

    Add files via upload

---
+++ equipe-data.js
@@ -0,0 +1,77 @@
+// MODIFICATION DE L'ÉQUIPE DEPUIS GITHUB
+// Pour changer une photo : ajoutez la photo dans assets/team/ puis indiquez son chemin dans photo.
+
+window.TEAM_DATA = [
+  {
+    group: 'Présidence de l’association',
+    type: 'president',
+    name: 'Pierre Bacqué',
+    role: 'Président de l’association',
+    initial: 'P',
+    photo: '',
+    text: 'J’accompagne les orientations de l’association Essaim Gâtinais et je veille aux valeurs de respect, de solidarité, de dignité et d’équité.'
+  },
+  {
+    group: 'Équipe du bureau',
+    type: 'bureau',
+    name: 'Sophie Leudière',
+    role: 'Directrice',
+    initial: 'D',
+    photo: '',
+    text: 'Je veille au bon fonctionnement du SIAMPADH, au suivi de la qualité des prestations et à l’écoute des bénéficiaires, des familles et des professionnels.'
+  },
+  {
+    group: 'Équipe du bureau',
+    type: 'bureau',
+    name: 'Eddy Lila',
+    role: 'Responsable de secteur',
+    initial: 'R',
+    photo: '',
+    text: 'J’organise les plannings, je coordonne les interventions à domicile, je suis les besoins des bénéficiaires et j’accompagne les équipes terrain.'
+  },
+  {
+    group: 'Portage de repas',
+    type: 'livreur',
+    name: 'Thomas',
+    role: 'Livreur de repas',
+    initial: 'T',
+    photo: '',
+    text: 'Chaque jour, je livre les repas aux bénéficiaires du SIAMPADH. Au-delà de la livraison, mon passage est un moment d’échange, d’écoute et de vigilance. Je veille à ce que chacun reçoive son repas dans les meilleures conditions.'
+  },
+  {
+    group: 'Les petits mots des intervenants',
+    type: 'intervenante',
+    name: 'Intervenante 1',
+    role: 'Aide au quotidien',
+    initial: '1',
+    photo: '',
+    text: 'J’accompagne les bénéficiaires dans les actes essentiels de la vie quotidienne : aide au lever, aide au coucher, habillage, repas et présence rassurante.'
+  },
+  {
+    group: 'Les petits mots des intervenants',
+    type: 'intervenante',
+    name: 'Intervenante 2',
+    role: 'Entretien du domicile',
+    initial: '2',
+    photo: '',
+    text: 'J’interviens pour l’entretien courant du logement, le linge, le repassage et les petites tâches du quotidien afin de préserver un cadre de vie agréable.'
+  },
+  {
+    group: 'Les petits mots des intervenants',
+    type: 'intervenante',
+    name: 'Intervenante 3',
+    role: 'Accompagnement extérieur',
+    initial: '3',
+    photo: '',
+    text: 'J’accompagne les bénéficiaires aux courses, aux rendez-vous, en promenade ou lors de sorties afin de maintenir l’autonomie et le lien social.'
+  },
+  {
+    group: 'Les petits mots des intervenants',
+    type: 'intervenante',
+    name: 'Intervenante 4',
+    role: 'Présence et vigilance',
+    initial: '4',
+    photo: '',
+    text: 'Je suis attentive au bien-être des bénéficiaires lors de chaque intervention. Mon rôle est d’apporter une aide concrète, une présence rassurante et une écoute bienveillante.'
+  }
+];
